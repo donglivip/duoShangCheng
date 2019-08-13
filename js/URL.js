fae4cfe45151434dc64942bb07155392bb8c67c1
dongxiaoli <1076454380@qq.com>
2019-08-13 17:36:14 +0800

    bug

--- js/URL.js
+++ js/URL.js
@@ -1,3 +1,3 @@
-var myurl='http://192.168.2.107'
+// var myurl='http://192.168.2.153'
    // var myurl='http://39.107.70.18:8088'
-   // var myurl = 'http://47.97.195.13'
+   var myurl = 'http://47.97.195.13'
